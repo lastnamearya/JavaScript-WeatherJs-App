f7b1dbc74c8252c52c28e446a5928b9ec16b3608
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-14 16:47:37 +0530

    Done with the Fetch ~ underground API

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 // Init weather object
 const weather = new Weather('Boston', 'MA');
 
-weather.getWeather()
+// Get weather on DOM Load
+document.addEventListener('DOMContentLoaded', getWeather);
+
+
+// weather.changeLocation('Miami', 'FL');
+
+function getWeather() {
+  weather.getWeather()
   .then(results => {
     console.log(results);
   })
-  .catch(err => console.log(results));
+  .catch(err => console.log(results)); 
+}
