e46a0e01c9ae63944bf357874a75da3f3f76d02f
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-14 18:50:06 +0530

    Finally we can change the location

--- app.js
+++ app.js
@@ -7,6 +7,20 @@ const ui = new UI();
 // Get weather on DOM Load
 document.addEventListener('DOMContentLoaded', getWeather);
 
+// Change Location Event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+  const state = document.getElementById('state').value;
+
+  weather.changeLocation('Miami', 'FL');
+
+  // Get and Display weather
+  getWeather();
+
+  // Close Modal
+  $('#locModal').modal('hide');
+})
+
 
 // weather.changeLocation('Miami', 'FL');
 
