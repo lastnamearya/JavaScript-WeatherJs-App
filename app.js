bf02806852dd788e1be884bfdd4ff4fdafd9c929
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-14 16:42:24 +0530

    Test Drive of weatherground API

---
+++ app.js
@@ -0,0 +1,8 @@
+// Init weather object
+const weather = new Weather('Boston', 'MA');
+
+weather.getWeather()
+  .then(results => {
+    console.log(results);
+  })
+  .catch(err => console.log(results));
